5945082cf41ad54238d9c999620111d84e4088d9
ivgtr <gear.1112t.o@gmail.com>
2021-02-04 19:15:36 +0900

    :sparkles: add: ts化

---
+++ src/cli.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import meow from 'meow'
+import symphogear from '.'
+
+const cli = meow(
+  `
+	Usage
+	  $ symphogear-g [file]
+	Examples
+	  $ symphogear-g さな.mp3
+	  $ symphogear-g http://sanabutton.ojaru.jp/sound/%E3%81%9B%E3%82%93%E3%81%9B%E3%81%88%E3%81%A8%E3%81%AA%E3%81%84%E3%81%97%E3%82%87%E3%81%B0%E3%81%AA%E3%81%97/%E3%81%A4%E3%81%8E%E3%81%A4%E3%81%8E%E3%81%A4%E3%83%BC%E3%81%8E06.mp3
+`
+)
+
+symphogear(cli.input[0])
---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import fs from 'fs'
+import path from 'path'
+import crypto from 'crypto'
+import envPaths from 'env-paths'
+import execa from 'execa'
+import axios from 'axios'
+import makeDir from 'make-dir'
+import isUrl from 'is-url-superb'
+import findExec from 'find-exec'
+
+const paths = envPaths('symphogear')
+
+const PLAYERS = ['mplayer', 'afplay', 'mpg123', 'mpg321', 'play', 'omxplayer', 'aplay', 'cmdmp3']
+
+function findPlayer(player?: string) {
+  if (player && PLAYERS.includes(player)) {
+    return player
+  }
+  const execPlayer: string | null = findExec(PLAYERS)
+  if (execPlayer === null) {
+    throw new Error("Couldn't find a suitable audio playe")
+  }
+  return execPlayer
+}
+
+function toHash(file: string) {
+  const shasum = crypto.createHash('sha1')
+  shasum.update(file)
+  return shasum.digest('hex')
+}
+
+async function fetchFile(file: string) {
+  if (!isUrl(file)) {
+    return file
+  }
+
+  const filePath = path.resolve(paths.cache, toHash(file))
+
+  if (fs.existsSync(filePath)) {
+    return filePath
+  }
+
+  await makeDir(path.dirname(filePath))
+
+  await axios({
+    method: 'get',
+    url: file,
+    responseType: 'stream'
+  }).then(function (response) {
+    response.data.pipe(fs.createWriteStream(filePath))
+  })
+
+  return filePath
+}
+
+const symphogear = async (file: string, opts: { player?: string } = {}) => {
+  if (typeof file !== 'string') {
+    throw new TypeError(`Expected a string, got ${typeof file}`)
+  }
+  const player = findPlayer(opts.player)
+  const result = await execa(player, [await fetchFile(file)])
+  return result
+}
+
+export default symphogear
