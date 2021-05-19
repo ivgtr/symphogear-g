8903dfa750ad93be37040fddc95b22c21fa564fb
ivgtr <gear.1112t.o@gmail.com>
2021-05-19 12:46:31 +0900

    :package: update: パッケージ更新など

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,71 @@
-import fs from 'fs'
-import path from 'path'
-import crypto from 'crypto'
-import envPaths from 'env-paths'
-import execa from 'execa'
-import axios from 'axios'
-import makeDir from 'make-dir'
-import isUrl from 'is-url-superb'
-import findExec from 'find-exec'
+import axios from "axios";
+import crypto from "crypto";
+import envPaths from "env-paths";
+import execa from "execa";
+import findExec from "find-exec";
+import fs from "fs";
+import isUrl from "is-url-superb";
+import makeDir from "make-dir";
+import path from "path";
 
-const paths = envPaths('symphogear')
+const paths = envPaths("symphogear");
 
-const PLAYERS = ['mplayer', 'afplay', 'mpg123', 'mpg321', 'play', 'omxplayer', 'aplay', 'cmdmp3']
+const PLAYERS = ["mplayer", "afplay", "mpg123", "mpg321", "play", "omxplayer", "aplay", "cmdmp3"];
 
 function findPlayer(player?: string) {
   if (player && PLAYERS.includes(player)) {
-    return player
+    return player;
   }
-  const execPlayer: string | null = findExec(PLAYERS)
+  const execPlayer: string | null = findExec(PLAYERS);
   if (execPlayer === null) {
-    throw new Error("Couldn't find a suitable audio playe")
+    throw new Error("Couldn't find a suitable audio playe");
   }
-  return execPlayer
+  return execPlayer;
 }
 
 function toHash(file: string) {
-  const shasum = crypto.createHash('sha1')
-  shasum.update(file)
-  return shasum.digest('hex')
+  const shasum = crypto.createHash("sha1");
+  shasum.update(file);
+  return shasum.digest("hex");
 }
 
 async function fetchFile(file: string) {
   if (!isUrl(file)) {
-    return file
+    return file;
   }
 
-  const filePath = path.resolve(paths.cache, toHash(file))
+  const filePath = path.resolve(paths.cache, toHash(file));
 
   if (fs.existsSync(filePath)) {
-    return filePath
+    return filePath;
   }
 
-  await makeDir(path.dirname(filePath))
+  await makeDir(path.dirname(filePath));
 
   await axios({
-    method: 'get',
+    method: "get",
     url: file,
-    responseType: 'stream'
+    responseType: "stream",
   })
-    .then(function (response) {
-      response.data.pipe(fs.createWriteStream(filePath))
+    .then((response) => {
+      response.data.pipe(fs.createWriteStream(filePath));
     })
     .catch(() => {
       if (fs.existsSync(filePath)) {
-        fs.unlink(filePath, () => {})
+        fs.unlink(filePath, () => {});
       }
-    })
+    });
 
-  return filePath
+  return filePath;
 }
 
 const symphogear = async (file: string, opts: { player?: string } = {}) => {
-  if (typeof file !== 'string') {
-    throw new TypeError(`Expected a string, got ${typeof file}`)
+  if (typeof file !== "string") {
+    throw new TypeError(`Expected a string, got ${typeof file}`);
   }
-  const player = findPlayer(opts.player)
-  const result = await execa(player, [await fetchFile(file)])
-  return result
-}
+  const player = findPlayer(opts.player);
+  const result = await execa(player, [await fetchFile(file)]);
+  return result;
+};
 
-export default symphogear
+export default symphogear;
